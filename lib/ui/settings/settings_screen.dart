ab21503a0377fe78f91a7bb7a4f312f05056fe1d
suragch <suragch@suragch.dev>
2026-03-12 13:30:03 +0800

    settings screen

---
+++ lib/ui/settings/settings_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Theme'),
+            subtitle: Text('Light mode'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
